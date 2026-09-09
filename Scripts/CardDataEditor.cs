f3f50e6abd35728bfb066afaa186e47a769cea81
AALUND13 <58748165+AALUND13@users.noreply.github.com>
2026-09-09 02:50:48 -0700

    Update version for Thunderstore

--- Scripts/CardDataEditor.cs
+++ Scripts/CardDataEditor.cs
@@ -30,7 +30,7 @@ namespace CardDataEditor {
         public const string ModId = "AALUND13.Card.Data.Editor";
         public const string ModName = "Card Data Editor";
         public const string ModInitials = "CDE";
-        public const string Version = "1.0.3"; // What version are we on (major.minor.patch)?
+        public const string Version = "1.1.0"; // What version are we on (major.minor.patch)?
 
         public static CardDataEditor Instance { get; private set; }
         public static ManualLogSource ModLogger { get; private set; }
