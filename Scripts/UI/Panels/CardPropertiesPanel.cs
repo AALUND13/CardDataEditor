5d67034d5146988c526e1abc4b9df5516c580165
AALUND13 <58748165+AALUND13@users.noreply.github.com>
2026-09-14 18:48:15 -0700

    Update cards properties panel when properties values are changed

--- Scripts/UI/Panels/CardPropertiesPanel.cs
+++ Scripts/UI/Panels/CardPropertiesPanel.cs
@@ -1,10 +1,26 @@
 ﻿using CardDataEditor.DataEditting.Config;
+using CardDataEditor.DataEditting.Properties;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
+using UnboundLib.Utils;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace CardDataEditor.UI.Panels {
     public class CardPropertiesPanel : MonoBehaviour {
+        private struct CardPropertyRef {
+            public CardPropertyConfigEntry CardProperty;
+            public GameObject ProperyUIObject;
+            public string Category;
+
+            public CardPropertyRef(CardPropertyConfigEntry cardProperty, GameObject properyUIObject, string category) {
+                CardProperty = cardProperty;
+                ProperyUIObject = properyUIObject;
+                Category = category;
+            }
+        }
+
         [Header("Prefabs")]
         public GameObject HeaderText;
 
@@ -12,27 +28,54 @@ namespace CardDataEditor.UI.Panels {
         public RectTransform PropertiesHolder;
 
 
-        private readonly Dictionary<string, List<GameObject>> CategoryToPropertiesMap
-            = new Dictionary<string, List<GameObject>>();
-        private readonly List<GameObject> CurrentlyActiveProperties
-            = new List<GameObject>();
+        private readonly Dictionary<string, List<CardPropertyRef>> CategoryToPropertiesMap
+            = new Dictionary<string, List<CardPropertyRef>>();
+
+        private readonly Dictionary<string, GameObject> CategoryHeaderMap
+            = new Dictionary<string, GameObject>();
+        private readonly Dictionary<CardPropertyConfigEntry, GameObject> PropertiesMap 
+            = new Dictionary<CardPropertyConfigEntry, GameObject>();
+
+        private CardOptionsConfigEntry cardOptionsConfigEntry;
 
 
-        public void CreateCardProperties(CardOptionsConfigCategory category) {
+        private void Update() {
+            LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform)PropertiesHolder);
+        }
+
+
+        public void CreateCardProperties(CardOptionsConfigEntry entry) {
+            if(cardOptionsConfigEntry != null)
+                cardOptionsConfigEntry.OnEntryChanged -= UpdateProperties;
+            
+            cardOptionsConfigEntry = entry;
+            cardOptionsConfigEntry.OnEntryChanged += UpdateProperties;
+
+            foreach (var property in CategoryHeaderMap)
+                Destroy(property.Value);
+            foreach (var property in PropertiesMap)
+                Destroy(property.Value);
+
             CategoryToPropertiesMap.Clear();
-            foreach (var property in CurrentlyActiveProperties) {
-                Destroy(property);
-            }
+            CategoryHeaderMap.Clear();
+            PropertiesMap.Clear();
 
-            foreach (var property in category.CardOptions.Properties.Values) {
-                if (property.CanShowProperty()) {
-                    string propertyCategory = property.GetCategoryName();
+
+            foreach (var property in entry.ConfigEntries) {
+                if (property.CardOptionProperty.CanShowProperty()) {
+                    string propertyCategory = property.CardOptionProperty.GetCategoryName();
                     if (!CategoryToPropertiesMap.ContainsKey(propertyCategory)) {
-                        CategoryToPropertiesMap.Add(propertyCategory, new List<GameObject>());
+                        CategoryToPropertiesMap.Add(propertyCategory, new List<CardPropertyRef>());
                     }
 
-                    var propertyUIObj = property.CreateUIProperty(category.GetEntry(property.GetSerializeName()));
-                    CategoryToPropertiesMap[propertyCategory].Add(propertyUIObj);
+                    var propertyUIObj = property.CardOptionProperty.CreateUIProperty(entry.GetEntry(property.CardOptionProperty.GetSerializeName()));
+                    CategoryToPropertiesMap[propertyCategory].Add(
+                        new CardPropertyRef(
+                            property,
+                            propertyUIObj,
+                            propertyCategory
+                        )
+                    );
                 }
             }
 
@@ -44,13 +87,46 @@ namespace CardDataEditor.UI.Panels {
                 headerObj.transform.SetParent(PropertiesHolder);
                 headerObj.transform.localScale = Vector3.one;
 
-                CurrentlyActiveProperties.Add(headerObj);
-                foreach (var propertyObj in propertiesCategory.Value) {
-                    propertyObj.transform.SetParent(PropertiesHolder);
-                    propertyObj.transform.localScale = Vector3.one;
-                    CurrentlyActiveProperties.Add(propertyObj);
+                CategoryHeaderMap.Add(propertiesCategory.Key, headerObj);
+                foreach (var property in propertiesCategory.Value) {
+                    property.ProperyUIObject.transform.SetParent(PropertiesHolder);
+                    property.ProperyUIObject.transform.localScale = Vector3.one;
+                    PropertiesMap.Add(property.CardProperty, property.ProperyUIObject);
                 }
+            }
 
+
+            
+        }
+
+        public void UpdateProperties(CardPropertyConfigEntry cardPropertyConfigEntry) {
+            bool isNewProperty = !PropertiesMap.ContainsKey(cardPropertyConfigEntry) && cardPropertyConfigEntry.CardOptionProperty.CanShowProperty();
+
+            if (isNewProperty) {
+                CreateCardProperties(cardOptionsConfigEntry);
+                return;
+            } else if(cardPropertyConfigEntry.CardOptionProperty.CanShowProperty()) {
+                return;
+            }
+
+            GameObject propertyObj = PropertiesMap[cardPropertyConfigEntry];
+
+            PropertiesMap.Remove(cardPropertyConfigEntry);
+            Destroy(propertyObj);
+
+            string category = cardPropertyConfigEntry.CardOptionProperty.GetCategoryName();
+
+            if (CategoryToPropertiesMap.TryGetValue(category, out var properties)) {
+                properties.RemoveAll(p => p.CardProperty == cardPropertyConfigEntry);
+
+                if (properties.Count == 0) {
+                    if (CategoryHeaderMap.TryGetValue(category, out var headerObj)) {
+                        Destroy(headerObj);
+                        CategoryHeaderMap.Remove(category);
+                    }
+
+                    CategoryToPropertiesMap.Remove(category);
+                }
             }
         }
     }
