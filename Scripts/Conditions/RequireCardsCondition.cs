3c8215c18ce3981c011d26058925842715899c22
AALUND13 <58748165+AALUND13@users.noreply.github.com>
2026-09-14 18:51:27 -0700

    Added card requirement and blacklist properties

---
+++ Scripts/Conditions/RequireCardsCondition.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CardDataEditor.Conditions {
+    public static class RequireCardsCondition {
+        public static Dictionary<CardInfo, List<CardInfo>> RequireCards = new Dictionary<CardInfo, List<CardInfo>>();
+
+        public static bool PlayerAllowedCard(Player player, CardInfo cardInfo) {
+            if (RequireCards.TryGetValue(cardInfo, out List<CardInfo> requireCards)) {
+                return requireCards.Count == 0 || requireCards.All(player.data.currentCards.Contains);
+            } else {
+                return true;
+            }
+        }
+    }
+}
